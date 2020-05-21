6d5fc223bf4ab2d0060ca9985d1470a4a06fa796
pratikmistry96 <pratikmistry08@gmail.com>
2020-05-20 23:15:01 -0400

    Add code to signals and initialize behavior code

---
+++ tlab/beh.py
@@ -0,0 +1,8 @@
+from scipy import signal
+import numpy as np
+
+def getvel(distance):
+    pass
+
+def unwrapencoder(wheel):
+    pass
--- tlab/sig.py
+++ tlab/sig.py
@@ -2,19 +2,52 @@ import numpy as np
 from scipy import signal
 
 def lpfilt(sig,fs=2000,ord=8,cutoff=10,type='butter'):
+    '''
+    Function to easily implement low pass filters.
+    The base parameters are for low pass filtering of
+    photometry signals
+    '''
     b, a = signal.iirfilter(ord,cutoff,btype='lowpass',
                             ftype=type,fs=fs)
     return signal.filtfilt(b, a, sig)
 
 def hpfilt(sig,fs=2000,ord=8,cutoff=1,type='butter'):
+    '''
+    Function to easily implemment high pass filters.
+    The base parameters are removing slow trends in
+    photometry signals
+    '''
     b, a = signal.iirfilter(ord,cutoff,btype='highpass',
                             ftype=type,fs=fs)
     return signal.filtfilt(b, a, sig)
 
 def bpfilt(sig,fs=30000,ord=8,cutoff_1=900,cutoff_2=9000,type='butter'):
+    '''
+    Function to easily implement band pass filters.
+    Base parameters are for band pass filtering
+    electrophysiology signals.
+    '''
     b, a = signal.iirfilter(ord,[cutoff_1,cutoff_2],btype='bandpass',
                             ftype=type,fs=fs)
     return signal.filtfilt(b, a, sig)
+
+def baseline(sig,window=10,prc=10,fs=2000):
+    '''
+    Function to detrend or baseline signals. The common
+    parameters are for baselining photometry signals
+    *INCOMPLETE*
+    '''
+    sig_length = np.size(sig)
+    window = window * fs
+    num_chunks = sig_length / window
+    base_pts = np.zeros((num_chunks,1))
+    for idx in range(num_chunks):
+        if idx == num_chunks - 1:
+            chunk = sig[idx*window::]
+        else:
+            chunk = sig[idx*window:((idx+1)*window)-1]
+        base_pts[idx] = np.percentile(chunk,prc)
+    pass
 
 def digLIA(sig,ref,fs,lpcut=10,ord=8,):
     pass
